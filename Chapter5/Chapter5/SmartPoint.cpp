7a2a93601a63c9a68b2aa9bc14a64fd25e635307
zxpKeda <zhangxiaopin@kedacom.com>
2018-11-19 14:01:26 +0800

    add shared memory solution

--- Chapter5/Chapter5/SmartPoint.cpp
+++ Chapter5/Chapter5/SmartPoint.cpp
@@ -82,3 +82,68 @@ void TsetShared_ptrByOtherDelete()
     shared_ptr<ofstream> fp(new ofstream("tmpfile.txt"), FileDeleter("tmpfile.txt"));
 }
 
+
+//////////////////////////////////////////////////////////////////////////
+// This function should be runed on Unix/Linux
+//class SharedMemoryDetacher
+//{
+//public:
+//    void operator () (int* p)
+//    {
+//        cout << "unlink /tmp1234" << endl;
+//        if (shm_unlink("/tmp1234") != 0)
+//        {
+//            cerr << "OOPS: shm_unlink() failed" << endl;
+//        }
+//    }
+//};
+//
+//shared_ptr<int> getSharedIntMemory(int num)
+//{
+//    void* mem;
+//
+//    int shmfd = shm_open("/tmp1234", O_CREAT | O_RDWR, S_IRWXU | S_IRWXG);
+//    if (shmfd < 0)
+//    {
+//        throw string(strerror(errno));
+//    }
+//
+//    if (ftrnucate(shmfd, num* sizeof(int) ) == -1 )
+//    {
+//        throw string(strerror(errno));
+//    }
+//
+//    mem = mmap(nullptr, num * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0);
+//    if (mem == MAP_FAILED)
+//    {
+//        throw string(strerror(errno));
+//    }
+//
+//    return shared_ptr<int>(static_cast<int*>(mem), SharedMemoryDetacher());
+//}
+//void TestShared_ptrBySharedMemory()
+//{
+//    //get and attach shared memory for 100 ints
+//    shared_ptr<int> smp(getSharedIntMemory(100));
+//
+//    //init the shared memory
+//    for (int i = 0; i < 100; ++i)
+//    {
+//        smp.get()[i] = i * 42;
+//    }
+//
+//    //deal with shared memory somewhere else:
+//    // ...
+//    cout << "<return>" << endl;
+//    cin.get();
+//
+//    //release shared memory here:
+//    smp.reset();
+//
+//    // ...
+//
+//}
+
+
+
+//////////////////////////////////////////////////////////////////////////
--- Chapter5/Chapter5/SmartPoint.h
+++ Chapter5/Chapter5/SmartPoint.h
@@ -3,3 +3,4 @@
 void TestShared_ptr();
 void TestShared_ptrByArray();
 void TsetShared_ptrByOtherDelete();
+//void TestShared_ptrBySharedMemory();
